50922df979dcac257803ce504016f757152e0200
magrowing <kmagrowing@gmail.com>
2024-05-10 11:36:19 +0900

    refactor : firebase 경로 error 처리

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import {fileURLToPath , URL} from 'url'
+
 
 export default defineConfig({
   plugins: [react()],
-  resolve : {
-    alias : {
-      '@' : fileURLToPath(new URL('./src', import.meta.url)),
-      '@assets' : fileURLToPath(new URL('./src/assets', import.meta.url)),
-      '@components' : fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@pages' : fileURLToPath(new URL('./src/pages', import.meta.url)),
-      '@types' : fileURLToPath(new URL('./src/types', import.meta.url)),
-      '@store' : fileURLToPath(new URL('./src/store', import.meta.url)),
-      '@apis' : fileURLToPath(new URL('./src/apis', import.meta.url)),
-      '@firebase' : fileURLToPath(new URL('./src/firebase', import.meta.url)),
-      '@utils' : fileURLToPath(new URL('./src/utils', import.meta.url)),
-      '@styles' : fileURLToPath(new URL('./src/styles', import.meta.url)),
-    }
-  }, 
 })
